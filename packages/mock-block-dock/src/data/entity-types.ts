36c58575e0289015b9a030f4115d8d4a426d9a10
Alfred Mountfield <alfie.mountfield@gmail.com>
2022-12-11 00:13:53 +0000

    Update demo entity types to be valid

--- packages/mock-block-dock/src/data/entity-types.ts
+++ packages/mock-block-dock/src/data/entity-types.ts
@@ -2,60 +2,51 @@ import { EntityType } from "@blockprotocol/graph";
 
 export const entityTypes: EntityType[] = [
   {
-    entityTypeId: "Company",
-    schema: {
-      $id: "https://example.com/types/Company",
-      title: "Company",
-
-      type: "object",
-      $schema: "https://json-schema.org/draft/2019-09/schema",
-      description: "A company or organisation.",
-      properties: {
-        employees: {
-          type: "number",
-          description: "The number of employees in the company.",
-        },
-        name: {
-          type: "string",
-          description: "A display name for the company.",
-        },
+    kind: "entityType",
+    $id: "https://example.com/types/entity-type/company/v/1",
+    type: "object",
+    title: "Company",
+    description: "A company or organization.",
+    properties: {
+      "https://example.com/types/property-type/employees/": {
+        $ref: "https://example.com/types/property-type/employees/v/1",
       },
-      labelProperty: "name",
-      required: ["name", "employees"],
+      "https://example.com/types/property-type/name/": {
+        $ref: "https://example.com/types/property-type/name/v/1",
+      },
     },
+    required: [
+      "https://example.com/types/property-type/employees/",
+      "https://example.com/types/property-type/name/",
+    ],
+    links: {},
   },
   {
-    entityTypeId: "Person",
-    schema: {
-      $id: "https://example.com/types/Person",
-      title: "Person",
-
-      type: "object",
-      $schema: "https://json-schema.org/draft/2019-09/schema",
-      description: "A human person.",
-      properties: {
-        age: {
-          type: "number",
-          description: "The age of the person, in years.",
-        },
-        email: {
-          type: "string",
-          description: "An email address.",
-          format: "email",
-        },
-        name: {
-          type: "string",
-          description: "The person's name.",
-        },
-        username: {
-          description: "The person's username in this application",
-          type: "string",
-          minLength: 4,
-          maxLength: 24,
-        },
+    kind: "entityType",
+    $id: "https://example.com/types/entity-type/person/v/1",
+    type: "object",
+    title: "Person",
+    description: "A human person.",
+    properties: {
+      "https://example.com/types/property-type/age/": {
+        $ref: "https://example.com/types/property-type/age/v/1",
       },
-      labelProperty: "name",
-      required: ["age", "email", "name", "username"],
+      "https://example.com/types/property-type/email/": {
+        $ref: "https://example.com/types/property-type/email/v/1",
+      },
+      "https://example.com/types/property-type/name/": {
+        $ref: "https://example.com/types/property-type/name/v/1",
+      },
+      "https://example.com/types/property-type/username/": {
+        $ref: "https://example.com/types/property-type/username/v/1",
+      },
     },
+    required: [
+      "https://example.com/types/property-type/age/",
+      "https://example.com/types/property-type/email/",
+      "https://example.com/types/property-type/name/",
+      "https://example.com/types/property-type/username/",
+    ],
+    links: {},
   },
 ];

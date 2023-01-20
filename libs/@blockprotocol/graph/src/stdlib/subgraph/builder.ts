d6bd04e927d775756bd04c795d48e71fb366f435
Alfred Mountfield <alfie.mountfield@gmail.com>
2023-01-20 15:19:33 +0000

    Add temporal axes to `buildSubgraph`

--- libs/@blockprotocol/graph/src/stdlib/subgraph/builder.ts
+++ libs/@blockprotocol/graph/src/stdlib/subgraph/builder.ts
@@ -3,6 +3,7 @@ import {
   Entity,
   EntityRecordId,
   GraphResolveDepths,
+  ResolvedQueryTemporalAxes,
   Subgraph,
 } from "../../types.js";
 
@@ -16,19 +17,23 @@ import {
  * The maximum value for any single depth is 255.
  *
  * This function does NOT verify that the provided depths are accurate for the data.
- *   – the caller is responsible for this.
+ * This function does NOT verify if provided temporal axes are accurate for the data.
+ *   - the caller is responsible for both of the above.
  * It DOES check that the provided roots are present in the data.
  *
  * @param data – the data to build the subgraph from (which becomes the vertices)
  * @param data.entities – the entities to build the subgraph from
  * @param depths – the depth values to provide in the returned subgraph
  * @param rootRecordIds – the root values to provide in the returned subgraph
+ * @param {ResolvedQueryTemporalAxes} temporalAxes - the temporal axes that were used when originally selecting the
+ *   provided data
  *
  * @returns a Subgraph containing:
  *   - 'vertices' containing the provided entities
  *   - 'edges' calculated by the function, representing connections between vertices
  *   - 'depths' as provided by the caller
  *   - 'roots' as provided by the caller
+ *   - 'temporalAxes' where both the `initial` and `resolved` are as provided by the caller
  *
  * @throws if the provided roots are not present in the data
  *
@@ -38,6 +43,7 @@ export const buildSubgraph = (
   data: { entities: Entity[] },
   rootRecordIds: EntityRecordId[],
   depths: GraphResolveDepths,
+  temporalAxes: ResolvedQueryTemporalAxes,
 ) => {
   const missingRoots = rootRecordIds.filter(
     ({ entityId, editionId }) =>
@@ -59,6 +65,12 @@ export const buildSubgraph = (
     );
   }
 
+  if (data.entities.length === 0 && temporalAxes === undefined) {
+    throw new Error(
+      "Unable to infer a valid set of temporal axes as there were no entities provided to be ",
+    );
+  }
+
   const roots = rootRecordIds.map((rootRecordId) => ({
     baseId: rootRecordId.entityId,
     /** @todo - This is temporary, and wrong */
@@ -70,6 +82,10 @@ export const buildSubgraph = (
     vertices: {},
     edges: {},
     depths,
+    temporalAxes: {
+      initial: temporalAxes,
+      resolved: temporalAxes,
+    },
   };
 
   addEntitiesToSubgraphByMutation(subgraph, data.entities);
